0c10adc270b067f42a1630fd6ca6d869c41057c9
Stefano Giordano <stefano.giordano@eng.it>
2022-09-18 17:24:14 +0200

    update

--- WebApplication1Prova/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebApplication1Prova.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
